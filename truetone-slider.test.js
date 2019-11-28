de006cd86f366df44a3c945c9bde0651c57b6262
Tony Thomas <thoma127@umn.edu>
2019-11-27 18:05:06 -0600

    Last test passes

--- truetone-slider.test.js
+++ truetone-slider.test.js
@@ -32,3 +32,22 @@ test(".value returns the slider input's value", () => {
   const subject = new slider(sliderElem);
   expect(subject.value).toEqual("440");
 });
+
+test("The value of the slider is rendered to the correct element", () => {
+  document.body.innerHTML =
+    '<label for="freqency-slider">Frequency (Hz)</label>' +
+    '<input id="freqency-slider"' +
+    '        class="input-range frequency"' +
+    '        type="range"' +
+    '        step="1"' +
+    '        value="440"' +
+    '        min="16"' +
+    '        max="880"' +
+    '        data-value-elem="frequency-slider-value">' +
+    '<p id="frequency-slider-value"></p>';
+  debugger;
+  const sliderElem =  document.querySelector(".input-range.frequency");
+  new slider(sliderElem).init();
+  const sliderValueElem =  document.getElementById("frequency-slider-value");
+  expect(sliderValueElem.innerHTML).toEqual("440");
+});
